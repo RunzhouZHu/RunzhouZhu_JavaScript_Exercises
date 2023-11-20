bd492c9a4857ffb9ce1cc07e62b4dbfc66af0537
Runzhou Zhu <runzhouz@metropolia.fi>
2023-11-20 14:42:17 +0200

    123

--- Module2/assignment10/JavaScript.js
+++ Module2/assignment10/JavaScript.js
@@ -1,17 +1,46 @@
-function even(list)
+// Candidates initialization.
+let numberOfCandidates = parseInt(prompt("Please enter the number of candidates: "))
+const voteCount = []
+
+for(let i=1;i<=numberOfCandidates;i++)
 {
-    const list1 = []
-    list.forEach(name =>{
-        if(name % 2 === 0)
-        {
-            list1.push(name)
-        }
-    })
-    return list1
+    let name = prompt(`Please enter the name of candidate ${i}: `);
+    let votes = 0 ;
+    const candidate = {name:name, votes:votes};
+    voteCount.push(candidate);
 }
 
-const list = [1,2,3,4,5,6,7,8,9,10]
-list1 = even(list)
+// Voters
+let numberOfVoters = parseInt(prompt("Please enter the number of voters: "))
+for(let i = 1;i<=numberOfVoters;i++)
+{
+    let vote = prompt("Please enter who you vote for: ")
+    voteCount.forEach(name =>{
+        if(vote === name.name)
+        {
+            name.votes++;
+        }
+    })
+}
 
-console.log(list)
-console.log(list1)
+// Compare
+const result = []
+result.push(voteCount[0])
+for(let i=0;i<voteCount.length;i++)
+{
+    if(result[0]<voteCount[i+1])
+    {
+        result.unshift(voteCount[i+1]);
+    }
+    else
+    {
+        result.push(voteCount[i+1]);
+    }
+}
+
+// Output
+console.log(`The winner is ${result[0].name} with ${result[0].votes} votes.`);
+console.log('results: ');
+result.forEach(name =>{
+    console.log(`${name.name} : ${name.votes} votes.`);
+})
