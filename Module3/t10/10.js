bc0c897bbfa5fdcfada7e00572554b38704abb7e
Runzhou Zhu <runzhouz@metropolia.fi>
2023-11-25 15:43:05 +0200

    123

--- Module3/t10/10.js
+++ Module3/t10/10.js
@@ -1,4 +1,14 @@
+    document.getElementById('source').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevents the form from submitting and refreshing the page
 
+        // Get input values
+        const firstName = document.querySelector('input[name="firstname"]').value;
+        const lastName = document.querySelector('input[name="lastname"]').value;
+
+        // Display submitted information
+        const targetElement = document.getElementById('target');
+        targetElement.textContent = 'Your name is ' + firstName + ' ' + lastName;
+    });
 
 
 
